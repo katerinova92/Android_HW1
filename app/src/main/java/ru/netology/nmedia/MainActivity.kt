859df4aecafb3179bbe0f65e7c57f7878f35b236
Ekaterina Safonova <katerina_s92@mail.ru>
2024-04-03 10:20:45 +0300

    OnClickListener added

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -2,10 +2,63 @@ package ru.netology.nmedia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.netology.nmedia.databinding.ActivityMainBinding
+import ru.netology.nmedia.dto.Post
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val post = Post(
+            id = 1,
+            author = "Нетология. Университет интернет-профессий будущего",
+            content = "Привет! Это новая Нетология! Привет! Это новая Нетология!",
+            published = "21 мая в 18:36",
+            likes = 1099999,
+            share = 999999,
+            likedByMe = false
+        )
+
+        with(binding) {
+            author.text = post.author
+            published.text= post.published
+            postText.text = post.content
+            likesNumber.text = post.likes.toString()
+            sharedNumber.text = post.share.toString()
+
+
+            if(post.likedByMe) {
+                icLiked?.setImageResource(R.drawable.ic_liked_24)
+            }
+
+            icLiked.setOnClickListener {
+                if(post.likedByMe) post.likes-- else post.likes++
+                post.likedByMe = !post.likedByMe
+                icLiked.setImageResource(
+                    if(post.likedByMe) R.drawable.ic_liked_24 else R.drawable.ic_like_24
+                )
+                likesNumber.text = CountChange(post.likes)
+            }
+
+            shared.setOnClickListener {
+                post.share++
+                sharedNumber.text = CountChange(post.share)
+            }
+        }
     }
-}
+
+    fun CountChange(count: Int): String {
+        var countToPost = count.toString()
+
+        when {
+            count > 1099999 && (count%1000000!=0) -> countToPost = "%.1f".format(count.toDouble()/1000000).toString() + "M"
+            count > 999999 -> countToPost = (count/1000000).toString() + "M"
+            count > 1099 && (count%1000!=0) -> countToPost = "%.1f".format(count.toDouble()/1000).toString() + "K"
+            count > 999 -> countToPost = (count/1000).toString() + "K"
+            else -> countToPost
+        }
+        return countToPost
+    }
+    }
---
+++ app/src/main/java/ru/netology/nmedia/dto/Post.kt
@@ -0,0 +1,12 @@
+package ru.netology.nmedia.dto
+
+data class Post(
+    val id: Long,
+    val author: String,
+    val content: String,
+    val published: String,
+    var likes: Int = 0,
+    var share: Int = 0,
+    var likedByMe: Boolean = false,
+    val isShared: Boolean = true
+)
